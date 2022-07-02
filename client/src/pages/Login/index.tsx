b839c63b21b85aa61f647042367521af36dfe93a
JoaokCruz <jvcruz2@hotmail.com>
2022-07-02 20:16:35 -0300

    login and register frontend updated

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,24 +1,49 @@
-import { Input } from "../../components/Input";
-import { Container, LoginFormContainer } from "./styles";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useAuth } from '../../hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import { Form } from "@unform/web";
-import { useRef } from "react";
-import { api } from "../../services/api";
-import { Link, useNavigate } from "react-router-dom";
-
-import icon from "../../assets/logo.webp";
-import { useAuth } from "../../hooks/auth";
+import { Input } from "../../components/Input";
 
 interface LoginFormData {
   login: string;
   password: string;
 }
 
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        ParkingSpot
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
 export function Login() {
-  const formRef = useRef(null);
+  const formRef = React.useRef(null);
+  const inputRef = React.useRef(null);
   const navigate = useNavigate();
   const { Login } = useAuth();
   const handleSubmit = async (data: LoginFormData) => {
-    console.log(data);
+    console.log(data.login +"----"+ data.password);
 
     try {
       await Login(data);
@@ -29,23 +54,64 @@ export function Login() {
   };
 
   return (
-    <Container>
-      <LoginFormContainer>
-        <img src={icon} style={{ width: "150px" }} alt="logo" />
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <Input label="Login" type="text" name="login" placeholder="Login" />
-          <Input
-            label="Senha"
-            type="password"
-            name="password"
-            placeholder="Senha"
-          />
-          <div style={{ display: "flex", flexDirection: "row-reverse" }}>
-            <button type="submit">Entrar</button>
-          </div>
-        </Form>
-        <Link to="/register">Registre-se aqui</Link>
-      </LoginFormContainer>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://bandnewsfm.band.uol.com.br/wp-content/uploads/2019/03/Estacionamento.jpg)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              ParkingSpot
+            </Typography>
+            <Form ref={formRef} onSubmit={handleSubmit}>
+            <Input label="Login" type="text" name="login" placeholder="Login" />
+            <Input label="Senha" type="password" name="password" placeholder="Senha" />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Entrar
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                </Grid>
+                <Grid item>
+                  <Link href="/register" variant="body2">
+                    {"Registrar-se"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Form>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 }
--- client/src/pages/Register/index.tsx
+++ client/src/pages/Register/index.tsx
@@ -1,9 +1,22 @@
-import { Input } from "../../components/Input";
-import { Container, LoginFormContainer } from "./styles";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Form } from "@unform/web";
-import { useRef } from "react";
-import { api } from "../../services/api";
-import { Link, useNavigate } from "react-router-dom";
+import { Input } from "../../components/Input";
+import { useNavigate } from 'react-router-dom';
+import { useRef } from 'react';
+import { api } from '../../services/api';
 
 interface LoginFormData {
   email: string;
@@ -12,6 +25,21 @@ interface LoginFormData {
   login: string;
   password: string;
 }
+
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
 
 export function Register() {
   const formRef = useRef(null);
@@ -38,30 +66,77 @@ export function Register() {
   };
 
   return (
-    <Container>
-      <LoginFormContainer>
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <Input label="Nome" type="text" name="name" placeholder="Nome" />
-          <Input label="Email" type="text" name="email" placeholder="Email" />
-          <Input
-            label="Celular"
-            type="text"
-            name="phoneNumber"
-            placeholder="Celular"
-          />
-          <Input label="Login" type="text" name="login" placeholder="Login" />
-          <Input
-            label="Senha"
-            type="password"
-            name="password"
-            placeholder="Senha"
-          />
-          <div style={{ display: "flex", flexDirection: "row-reverse" }}>
-            <button type="submit">Entrar</button>
-          </div>
-        </Form>
-        <Link to="/login">Faça login</Link>
-      </LoginFormContainer>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://bandnewsfm.band.uol.com.br/wp-content/uploads/2019/03/Estacionamento.jpg)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Faça seu registro
+            </Typography>
+            <Form ref={formRef} onSubmit={handleSubmit}>
+            <Input label="Nome" type="text" name="name" placeholder="Nome" />
+            <Input label="Email" type="text" name="email" placeholder="Email" />
+            <Input
+              label="Celular"
+              type="text"
+              name="phoneNumber"
+              placeholder="Celular"
+            />
+            <Input label="Login" type="text" name="login" placeholder="Login" />
+            <Input
+              label="Senha"
+              type="password"
+              name="password"
+              placeholder="Senha"
+            />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Registrar-se
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                </Grid>
+                <Grid item>
+                  <Link href="/login" variant="body2">
+                    {"Faça o Login"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Form>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 }
